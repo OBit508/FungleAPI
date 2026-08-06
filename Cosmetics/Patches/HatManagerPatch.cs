b5c97a3785cc05e257d6c7c4f645685a3b415084
neon <174992783+neonmodder123@users.noreply.github.com>
2026-08-06 12:33:28 +0300

    Update HatManagerPatch.cs

--- Cosmetics/Patches/HatManagerPatch.cs
+++ Cosmetics/Patches/HatManagerPatch.cs
@@ -13,6 +13,34 @@ namespace FungleAPI.Cosmetics.Patches
     {
         public static void Postfix(HatManager __instance)
         {
+            if (CosmeticManager.UnlockAll && Constants.GetPlatformType() != Platforms.Android)
+            {
+                foreach (HatData hat in __instance.allHats)
+                {
+                    hat.Free = true;
+                }
+
+                foreach (NamePlateData nameplate in __instance.allNamePlates)
+                {
+                    nameplate.Free = true;
+                }
+
+                foreach (PetData pet in __instance.allPets)
+                {
+                    pet.Free = true;
+                }
+
+                foreach (SkinData skin in __instance.allSkins)
+                {
+                    skin.Free = true;
+                }
+
+                foreach (VisorData visor in __instance.allVisors)
+                {
+                    visor.Free = true;
+                }
+            }
+
             SetHat(__instance);
             SetSkin(__instance);
             SetVisor(__instance);
